e7799fb07db21203c4ac434b71461410af4e9a21
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-01-22 02:31:28 -0500

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -7,8 +7,8 @@ def pin_to_ipfs(data):
 	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 
 	headers = {
-        "pinata_api_key": "588669fdc80fc9ba4b68",
-        "pinata_secret_api_key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJmYmNkZDQwMi05ZjI3LTRjOTYtYjE4NC0zOGNkNDVmOTkzMDMiLCJlbWFpbCI6InNvb3lvdWxAc2Vhcy51cGVubi5lZHUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiNTg4NjY5ZmRjODBmYzliYTRiNjgiLCJzY29wZWRLZXlTZWNyZXQiOiI1ZTgzMmU4MGI3MmFlODc4YzQ0NTY1Zjc2ZmVmMWQxNjQzM2M0MWQwMTlhZDFkZDRkZGQ5Y2EyNjY5ZjQ5ZDJjIiwiaWF0IjoxNzA1OTA1MjQ3fQ.TDGX-LrFWCzB5iTpd0txPaR-Bx3jfl2e8w1i3v9dESw"
+        "pinata_api_key": "4d1ca1406d39d453fb23",
+        "pinata_secret_api_key": "102a7edffa23ac45f84fd12813211f9de296f9500341e7dfcfc0590d0d3486f5"
     }
 
 	response = requests.post(url, headers=headers)
