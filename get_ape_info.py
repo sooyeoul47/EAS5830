6dda55a4ff151d48725e87990373df8e5c59201e
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-01-28 17:53:31 -0500

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -8,7 +8,7 @@ import time
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.to_checksum_address(bayc_address)
-api_url = "https://gateway.pinata.cloud/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/"
+api_url = "https://gateway.pinata.cloud/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 #You will need the ABI to connect to the contract
