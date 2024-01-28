ef5e9c071fb0e9be21641a7a07cbe2419d1e68b0
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-01-28 14:54:53 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -10,7 +10,7 @@ def sign(m):
 
     account = w3.eth.account.create()
     eth_address = account.address  # Eth account
-    private_key = account.privateKey
+    private_key = account.key
 
     # generate signature
     # your code here
