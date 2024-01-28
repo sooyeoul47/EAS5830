b86358c6159f8005d452e69e45235c683c625751
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-01-28 15:58:11 -0500

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -14,7 +14,7 @@ web3 = Web3(provider)
 
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-contract_address = web3.utils.toChecksumAddress(bayc_address)
+contract_address = Web3.utils.toChecksumAddress(bayc_address)
 
 #You will need the ABI to connect to the contract
 #The file 'abi.json' has the ABI for the bored ape contract
