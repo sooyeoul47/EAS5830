eebf015ec4aac504b64465cdcd00fe1424dd652d
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-02-05 17:36:48 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -37,50 +37,12 @@ def verifySig():
 
 
 
-# Replace these with your actual details
-rpc_url = "https://api.avax-test.network/ext/bc/C/rpc"  
-private_key = "0xbe83d012497ec952d06a6096de569d1382321789f4719b099bb5d8d0d40d9cd0" 
-contract_address = "0x85ac2e065d4526FBeE6a2253389669a12318A412"
-# Web3.to_checksum_address(
-account = Account.from_key(private_key)
-from_address = account.address
 
-
-# Load ABI
-with open('/home/codio/workspace/NFT.abi', 'r') as abi_definition:
-    contract_abi = json.load(abi_definition)
-
-# Setup web3 connection
-w3 = Web3(HTTPProvider(rpc_url))
-
-# Load contract
-contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-
-def mint_nft():
-    nonce = w3.eth.get_transaction_count(from_address)
-    print(contract.functions.claim(from_address, Web3.to_bytes(text="nonce2")))
-    txn = contract.functions.claim(from_address, Web3.to_bytes(text="nonce2")).build_transaction({
-        'from': from_address,
-        'chainId': 43113,  # Chain ID for Avalanche Fuji Testnet
-        'gas': 20000,
-        'maxFeePerGas': w3.to_wei('50', 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei('1', 'gwei'),
-        'nonce': nonce,
-    })
-
-    signed_txn = w3.eth.account.sign_transaction(txn, private_key)
-    txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    print(f"Transaction hash: {txn_hash.hex()}")
-
-    receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
-    print(f"Transaction receipt: {receipt}")
 
 if __name__ == '__main__':
     """
         Test your function
     """
-    
-    mint_nft()
 
 
     if verifySig():
