9ec61c96e5e202171e88a12c4725a141afda4738
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-02-05 00:17:12 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,5 +1,4 @@
 from web3 import Web3
-from web3.contract import Contract
 from eth_account.messages import encode_defunct
 import random
 import os
@@ -37,7 +36,7 @@ def verifySig():
 
 bayc_address = "0x85ac2e065d4526FBeE6a2253389669a12318A412"
 contract_address = Web3.to_checksum_address(bayc_address)
-web3 = Web3(Web3.HTTPProvider("https://testnet.snowtrace.io/address/0xDEdA37C517eF097c10D6501A33de377F194660a5"))
+web3 = Web3(Web3.HTTPProvider("https://api.avax-test.network/ext/bc/C/rpc"))
 
 
 with open('/home/codio/workspace/NFT.abi', 'r') as f:
@@ -45,18 +44,19 @@ with open('/home/codio/workspace/NFT.abi', 'r') as f:
      
 nft_contract = web3.eth.contract(address=contract_address, abi=abi)
 
-def claim_nft(account):
-    nonce = os.urandom(16)  # Generate a random nonce
-    # Prepare transaction
-    tx = nft_contract.functions.claim(web3.toHex(nonce)).buildTransaction({
+def claim_nft(private_key):
+    account = web3.eth.account.from_key(private_key)
+    nonce = web3.eth.getTransactionCount(account.address)
+    tx = nft_contract.functions.claim(os.urandom(16)).buildTransaction({
         'from': account.address,
         'gas': 1000000,
         'gasPrice': web3.toWei('50', 'gwei'),
-        'nonce': web3.eth.getTransactionCount(account.address),
+        'nonce': nonce,
     })
-    signed_tx = web3.eth.account.sign_transaction(tx, account.key)
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    return web3.toHex(tx_hash)
+    print(f"Transaction hash: {tx_hash.hex()}")
+    return tx_hash
 
 if __name__ == '__main__':
     """
@@ -64,9 +64,7 @@ if __name__ == '__main__':
     """
     private_key = "0xbe83d012497ec952d06a6096de569d1382321789f4719b099bb5d8d0d40d9cd0"
 
-    account = web3.eth.account.from_key(private_key)
-
-    claim_nft(account)
+    claim_nft(private_key)
 
     if verifySig():
         print( f"You passed the challenge!" )
