5e096d5a353fdb445cc00aaecef13895ccdb2111
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-02-05 00:06:40 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,6 +1,9 @@
 from web3 import Web3
+from web3.contract import Contract
 from eth_account.messages import encode_defunct
 import random
+import os
+import json
 
 def signChallenge( challenge ):
 
@@ -31,10 +34,40 @@ def verifySig():
 
     return w3.eth.account.recover_message( challenge , signature=sig ) == address
 
+
+bayc_address = "0x85ac2e065d4526FBeE6a2253389669a12318A412"
+contract_address = Web3.to_checksum_address(bayc_address)
+web3 = Web3(Web3.HTTPProvider("https://testnet.snowtrace.io/address/0xDEdA37C517eF097c10D6501A33de377F194660a5"))
+
+
+with open('/home/codio/workspace/NFT.json', 'r') as f:
+	abi = json.load(f) 
+     
+nft_contract = Web3.eth.contract(address=contract_address, abi=abi)
+
+def claim_nft(account):
+    nonce = os.urandom(16)  # Generate a random nonce
+    # Prepare transaction
+    tx = nft_contract.functions.claim(web3.toHex(nonce)).buildTransaction({
+        'from': account.address,
+        'gas': 1000000,
+        'gasPrice': web3.toWei('50', 'gwei'),
+        'nonce': web3.eth.getTransactionCount(account.address),
+    })
+    signed_tx = web3.eth.account.sign_transaction(tx, account.key)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    return web3.toHex(tx_hash)
+
 if __name__ == '__main__':
     """
         Test your function
     """
+    private_key = "0xbe83d012497ec952d06a6096de569d1382321789f4719b099bb5d8d0d40d9cd0"
+
+    account = web3.eth.account.from_key(private_key)
+
+    claim_nft(account)
+
     if verifySig():
         print( f"You passed the challenge!" )
     else:
