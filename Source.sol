562721cc7fff8ef8c5bd95bbee7426bc5fdaf452
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-02-17 18:08:17 -0500

    Update Source.sol

--- Source.sol
+++ Source.sol
@@ -21,23 +21,23 @@ contract Source is AccessControl {
 
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-		require(registeredTokens[_token], "Token not registered");
-		require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
-		emit Deposit(_token, _recipient, _amount);
+		require(approved[_token], "Token not registered");
+        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        emit Deposit(_token, _recipient, _amount);
 	}
 
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
 		//YOUR CODE HERE
 		require(IERC20(_token).transfer(_recipient, _amount), "Transfer failed");
-		emit Withdrawal(_token, _recipient, _amount);
+        emit Withdrawal(_token, _recipient, _amount);
 	}
 
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
 		//YOUR CODE HERE
-		require(!registeredTokens[_token], "Token already registered");
-		registeredTokens[_token] = true;
-		tokens.push(_token);
-		emit Registration(_token);
+		require(!approved[_token], "Token already registered");
+        approved[_token] = true;
+        tokens.push(_token);
+        emit Registration(_token);
 	}
 
 
