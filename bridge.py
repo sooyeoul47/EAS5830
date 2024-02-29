dcb798a18be8e800c82922e635f746e4936c84f3
Sooyoul Lee <31755896+sooyeoul47@users.noreply.github.com>
2024-02-29 11:20:15 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -67,18 +67,15 @@ def scanBlocks(chain):
     destination_contract = w3_dst.eth.contract(address=destination_contract_address, abi=dst_abi)
 
     
-    src_end_block = w3_src.eth.block_number
+    src_end_block = w3_src.eth.get_block_number()
     src_start_block = src_end_block - 5
-    dst_end_block = w3_dst.eth.block_number
+    dst_end_block = w3_dst.eth.get_block_number()
     dst_start_block = dst_end_block - 5
 
-    # print(dst_end_block)
-    # print(w3_dst.eth.get_block_number())
-    # print(dst_start_block)
     arg_filter = {}
     if chain == "source":  #Source
         
-        event_filter = source_contract.events.Deposit.create_filter(fromBlock=dst_start_block, toBlock = dst_end_block,argument_filters=arg_filter)
+        event_filter = source_contract.events.Deposit.create_filter(fromBlock=src_start_block, toBlock = src_end_block, argument_filters=arg_filter)
         for event in event_filter.get_all_entries():
 
             txn = destination_contract.functions.wrap(event.args['token'], event.args['recipient'], event.args['amount']).build_transaction({
@@ -94,7 +91,7 @@ def scanBlocks(chain):
 
     elif chain == "destination":  #Destination
         
-        event_filter = destination_contract.events.Unwrap.create_filter(fromBlock=src_start_block, toBlock = src_start_block,argument_filters=arg_filter)
+        event_filter = destination_contract.events.Unwrap.create_filter(fromBlock=dst_start_block, toBlock = dst_end_block, argument_filters=arg_filter)
         for event in event_filter.get_all_entries():
 
             txn = source_contract.functions.withdraw(event.args['underlying_token'], event.args['to'], event.args['amount']).build_transaction({
@@ -107,4 +104,3 @@ def scanBlocks(chain):
             })
             signed_txn = w3_src.eth.account.sign_transaction(txn, private_key=private_key)
             w3_src.eth.send_raw_transaction(signed_txn.rawTransaction)
-
